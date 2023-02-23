5608ea8cb98882b269a121708a77fd74f41cbc03
Daria Yakkola <yakkolabest@gmail.com>
2023-02-23 12:54:09 +0300

    Задача 25

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = A;
+for (int i = 1 ; i < B; i++)
+{
+  result = result * A;
+}
+Console.WriteLine($"Число {A} в степени {B} равно {result} ");
